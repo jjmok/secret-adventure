0ec8e5a3ba8503c3aeb6ecaa2b97baa8ef459698
jeejin <jeejin@piktochart.com>
2018-02-20 22:34:06 +0800

    wip

--- js/index.js
+++ js/index.js
@@ -1,13 +1,48 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   const enterName = document.querySelector('#js-enter-name')
+  const previewBtn = document.querySelector('#js-preview-btn')
+  const blankNameError = document.querySelector('#js-name-blank-error')
+  const invalidNameError = document.querySelector('#js-name-invalid-error')
+
+  function addError (elem) {
+    elem.classList.add('c-input--error')
+  }
+
+  function resetError () {
+    blankNameError.classList.add('u-hide')
+    invalidNameError.classList.add('u-hide')
+    enterName.classList.remove('c-input--error')
+  }
+
+  previewBtn.addEventListener('mousedown', function (e) {
+    let target = enterName
+    let targetVal = target.value
+    resetError()
+
+    console.log(e.currentTarget);
+
+    if (targetVal === '') {
+      addError(enterName)
+      blankNameError.classList.remove('u-hide')
+      console.log('no name')
+    } else if (!/^[a-zA-Z]*$/g.test(targetVal)) {
+      addError(enterName)
+      invalidNameError.classList.remove('u-hide')
+      console.log('Invalid')
+    }
+  })
 
   enterName.addEventListener('blur', function (e) {
-    const target = e.currentTarget
+    let target = e.currentTarget
+    let targetVal = target.value
+    resetError()
 
-    if (typeof target.value === 'number') {
-      console.log('wrong')
+    if (!/^[a-zA-Z]*$/g.test(targetVal)) {
+      addError(enterName)
+      invalidNameError.classList.remove('u-hide')
+      console.log('Invalid')
     }
-    console.log('right')
 
+    // console.log(targetValue)
   })
 })
